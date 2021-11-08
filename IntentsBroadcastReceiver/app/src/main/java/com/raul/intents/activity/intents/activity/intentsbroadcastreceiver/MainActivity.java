37b6d98087d18e100d17a19cf927eb80ab82fe22
TheLordRalph <raulete10m@gmail.com>
2021-11-08 10:35:50 +0100

    first commit

---
+++ IntentsBroadcastReceiver/app/src/main/java/com/raul/intents/activity/intents/activity/intentsbroadcastreceiver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raul.intents.activity.intents.activity.intentsbroadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
